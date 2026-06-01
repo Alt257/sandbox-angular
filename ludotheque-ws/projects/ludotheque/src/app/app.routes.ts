6ae1e42d4e3b5f53d22bcab38d8971210508c364
benoît <bra@synapsia.fr>
2026-06-01 09:39:59 -0400

    Add route for `ListVideoGames` feature component

--- ludotheque-ws/projects/ludotheque/src/app/app.routes.ts
+++ ludotheque-ws/projects/ludotheque/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {ListVideoGames} from './features/video-games/components/list-video-games/list-video-games';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'video-games',
+    component: ListVideoGames,
+  },
+];
