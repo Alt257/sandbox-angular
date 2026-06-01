0eefcdf3b6f14907d97c37e8043285abf676eb00
benoît <bra@synapsia.fr>
2026-06-01 10:34:38 -0400

    Integrate `AsyncPipe` and bind `videoGames$` to the template in `ListVideoGames` component

--- ludotheque-ws/projects/ludotheque/src/app/features/video-games/components/list-video-games/list-video-games.ts
+++ ludotheque-ws/projects/ludotheque/src/app/features/video-games/components/list-video-games/list-video-games.ts
@@ -1,16 +1,20 @@
 import {Component, inject, signal} from '@angular/core';
 import {GetAllVideoGames} from '../../services/get-all-video-games';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'ldk-list-video-games',
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './list-video-games.html',
   styleUrl: './list-video-games.css',
 })
 export class ListVideoGames {
   private readonly getAllVideoGames = inject(GetAllVideoGames);
-  private readonly videoGames$ = this.getAllVideoGames.getAll();
-  readonly title = signal('Mes jeux');
+
+  protected readonly videoGames$ = this.getAllVideoGames.getAll();
+  protected readonly title = signal('Mes jeux');
 
   protected changeTitle() {
     this.title.set('Mes jeux-vidéos');
