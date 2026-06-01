8a4ffe498990259624a2d23fe9112c8e4a3a6165
benoît <bra@synapsia.fr>
2026-06-01 09:34:52 -0400

    Integrate `MainMenu` component into app with routing links and update `app.html`.

--- ludotheque-ws/projects/ludotheque/src/app/shared/uis/menus/main-menu/main-menu.ts
+++ ludotheque-ws/projects/ludotheque/src/app/shared/uis/menus/main-menu/main-menu.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'ldk-main-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-menu.html',
   styleUrl: './main-menu.css',
 })
