51a8104aeb611856ff043e46be6780d9705b4ec3
benoît <bra@synapsia.fr>
2026-06-01 10:17:27 -0400

    Add `getAll` method to `GetAllVideoGames` service with `HttpClient` injection

--- ludotheque-ws/projects/ludotheque/src/app/features/video-games/services/get-all-video-games.ts
+++ ludotheque-ws/projects/ludotheque/src/app/features/video-games/services/get-all-video-games.ts
@@ -1,6 +1,15 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {VideoGame, VideoGameResponse} from '../models/video-game';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GetAllVideoGames {}
+export class GetAllVideoGames {
+  private readonly httpClient = inject(HttpClient);
+
+  getAll(): Observable<VideoGameResponse> {
+    return this.httpClient.get<VideoGameResponse>('https://swapi.dev/api/films');
+  }
+}
