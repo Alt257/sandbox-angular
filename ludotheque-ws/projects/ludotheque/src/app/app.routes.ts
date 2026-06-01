e20e362c6aff56314879a888b74e2e1a0b4ae6df
benoît <bra@synapsia.fr>
2026-06-01 09:46:59 -0400

    Add routes for `Home` component and redirect root path to `home`

--- ludotheque-ws/projects/ludotheque/src/app/app.routes.ts
+++ ludotheque-ws/projects/ludotheque/src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import {ListVideoGames} from './features/video-games/components/list-video-games/list-video-games';
+import {Home} from './pages/home/home';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
   {
     path: 'video-games',
     component: ListVideoGames,
