6c66749e302efe49a39abf810a15565b8a0c57de
benoît <bra@synapsia.fr>
2026-06-01 12:50:28 -0400

    Bind `videoGame` input to `EditVideoGame` and set up form with validation in the template.

--- ludotheque-ws/projects/ludotheque/src/app/features/video-games/components/edit-video-game/edit-video-game.ts
+++ ludotheque-ws/projects/ludotheque/src/app/features/video-games/components/edit-video-game/edit-video-game.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {VideoGame} from '../../models/video-game';
 
 @Component({
   selector: 'ldk-edit-video-game',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './edit-video-game.html',
   styleUrl: './edit-video-game.css',
 })
-export class EditVideoGame {}
+export class EditVideoGame {
+  readonly videoGame = input.required<VideoGame>();
+}
