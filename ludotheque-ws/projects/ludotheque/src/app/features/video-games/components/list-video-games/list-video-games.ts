6b6dc40b5274e596942767cdb3a4ee799ba8edaf
benoît <bra@synapsia.fr>
2026-06-01 10:02:33 -0400

    Add `GetAllVideoGames` service with injection and unit test

--- ludotheque-ws/projects/ludotheque/src/app/features/video-games/components/list-video-games/list-video-games.ts
+++ ludotheque-ws/projects/ludotheque/src/app/features/video-games/components/list-video-games/list-video-games.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {GetAllVideoGames} from '../../services/get-all-video-games';
 
 @Component({
   selector: 'ldk-list-video-games',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './list-video-games.html',
   styleUrl: './list-video-games.css',
 })
-export class ListVideoGames {}
+export class ListVideoGames {
+  private readonly getAllVideoGames = inject(GetAllVideoGames);
+}
