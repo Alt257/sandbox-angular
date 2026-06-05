cbe1a3416d1b3bf762d06a754f3917646b7bd692
benoît <bra@synapsia.fr>
2026-06-05 14:53:13 -0400

    Refactor `StarWarsFilms` page to use `ListStarWarsFilms` component for displaying films

--- ludotheque-ws/projects/ludotheque/src/app/pages/star-wars-films/star-wars-films.ts
+++ ludotheque-ws/projects/ludotheque/src/app/pages/star-wars-films/star-wars-films.ts
@@ -1,11 +1,13 @@
 import {Component, inject, signal} from '@angular/core';
 import {StarWarsFilmsManager} from '../../features/star-wars-films/services/star-wars-films-manager';
 import {AsyncPipe} from '@angular/common';
+import { ListStarWarsFilms } from '../../features/star-wars-films/components/list-star-wars-films/list-star-wars-films';
 
 @Component({
   selector: 'ldk-star-wars-films',
   imports: [
     AsyncPipe,
+    ListStarWarsFilms,
   ],
   templateUrl: './star-wars-films.html',
   styleUrl: './star-wars-films.css',
