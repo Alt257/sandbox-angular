11ef55dd701c9c16b871561fdc2f79de15052703
benoît <bra@synapsia.fr>
2026-06-05 15:16:07 -0400

    Revamp main menu: add styles, improve structure, and enhance accessibility

--- ludotheque-ws/projects/ludotheque/src/app/shared/uis/menus/main-menu/main-menu.ts
+++ ludotheque-ws/projects/ludotheque/src/app/shared/uis/menus/main-menu/main-menu.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'ldk-main-menu',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './main-menu.html',
   styleUrl: './main-menu.css',
 })
